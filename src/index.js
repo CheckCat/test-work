d59d6294242f3241ca3bcac8f2599bfcfcba214a
Vadim Sabirov <vadimsabirov228@gmail.com>
2021-02-10 15:28:35 +0500

    using fetch to server

--- src/index.js
+++ src/index.js
@@ -9,12 +9,18 @@ import './style/style.css'
 
 const frontURL = new URL(window.location.href)
 const hash = frontURL.searchParams.get('num')
-// const proxyURL = 'https://cors-anywhere.herokuapp.com/'
-const backURL = `http://xkcd.com/${hash ? hash+'/' : ''}info.0.json`
+const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+const backURL = `${proxyURL}https://xkcd.com/${hash ? hash+'/' : ''}info.0.json`
 const root = document.querySelector('#root')
 
 const AppModules = [Title, ControlElements, Img, Description, ControlElements, FullDate]
 FetchData(backURL, root, AppModules).catch(() => {
+  const elems = Array.from(root.children)
+  elems.forEach(item=>item.remove())
+
+  root.append(Error404())
+})
+FetchData(`https://xkcd.com/${hash ? hash+'/' : ''}info.0.json`, root, AppModules).catch(() => {
   const elems = Array.from(root.children)
   elems.forEach(item=>item.remove())
 
